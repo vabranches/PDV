379085af3a1dc28746f59dcd5c1eeb718d211829
Valter <valter_abranches@yahoo.com.br>
2016-03-05 10:44:54 -0300

    Atualizacao até as 10:44 - Activity de Cadastro

--- app/src/main/java/br/com/trainning/pdv/MainActivity.java
+++ app/src/main/java/br/com/trainning/pdv/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.trainning.pdv;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -43,9 +44,14 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(id == R.id.action_novo){
+            Intent intent = new Intent(MainActivity.this, CadastroNovoActivity.class);
+            startActivity(intent);
+        } else if(id == R.id.action_edit){
+            Intent intent = new Intent(MainActivity.this,CadastroNovoActivity.class);
+            startActivity(intent);
         }
+
 
         return super.onOptionsItemSelected(item);
     }
